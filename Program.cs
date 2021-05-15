bdcd819767595f91f5ed72e9fe34a56b8718da67
dk.raw <hello@dkatsios.tk>
2021-05-15 14:05:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result =  ReverseWords("dogecoin to the moon");
+            System.Console.WriteLine(result);
+        }
+        private static string ReverseWords(string str)
+        {
+            string[] p = str.Split(' ');
+            List<string> temp = new List<string>();
+            for (int i = p.Length - 1; i >= 0; i--)
+            {
+                temp.Add(p[i]);
+            }
+            return string.Join(" ", temp);
+        }
+    }
+}
